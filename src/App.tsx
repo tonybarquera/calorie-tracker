b1388d73171b6e1d0dc928284740e61d4ad0f733
Tony Barquera <barqueratony@gmail.com>
2024-03-30 20:29:45 -0600

    [ADD] Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1 className="text-4xl">Contador de Calorías</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="px-5 max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">Contador de Calorías</h1>
+        </div>
+      </header>
     </>
   )
 }
