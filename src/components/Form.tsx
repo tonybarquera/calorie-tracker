2a9a605c7d273ed74d24821ce994c57cc664bce1
Tony Barquera <barqueratony@gmail.com>
2024-03-30 21:53:20 -0600

    [ADD] Activity State

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,21 @@
+import { ChangeEvent, useState } from "react";
 import { categories } from "../data/categories";
+import type { Activity } from "../types";
 
 function Form() {
+  const [ activity, setActivity ] = useState<Activity>({
+    category: '1',
+    name: '',
+    calories: 0
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
+    setActivity({
+      ...activity,
+      [e.target.id]: e.target.id === 'name' ? e.target.value : +e.target.value 
+    });
+  }
+
   return (
     <form
       className="space-y-5 bg-white shadow-lg p-10 rounded-lg"
@@ -10,6 +25,8 @@ function Form() {
         <select
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
+          value={activity.category}
+          onChange={e => handleChange(e)}
         >
           {categories.map(category => (
             <option key={category.id} value={category.id}>
@@ -20,12 +37,14 @@ function Form() {
       </div>
 
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">Actividad:</label>
+        <label htmlFor="name" className="font-bold">Actividad:</label>
         <input
-          id="activity"
+          id="name"
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           type="text"
           placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+          value={activity.name}
+          onChange={e => handleChange(e)}
         />
       </div>
 
@@ -36,7 +55,9 @@ function Form() {
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           type="number"
           placeholder="Calorías ej. 300 o 500"
+          value={activity.calories}
           min={0}
+          onChange={e => handleChange(e)}
         />
       </div>
 
