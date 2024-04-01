4fec5a98cb8178536f1dc24a28be8f23379bbeb9
Tony Barquera <barqueratony@gmail.com>
2024-03-31 18:14:22 -0600

    [ADD] restart app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useEffect } from "react";
+import { useReducer, useEffect, useMemo } from "react";
 import { activityReducer, initialState } from "./reducers/activityReducer";
 import Form from "./components/Form";
 import ActivityList from "./components/ActivityList";
@@ -10,11 +10,14 @@ function App() {
     localStorage.setItem('activities', JSON.stringify(state.activities));
   }, [ state.activities ]);
 
+  const canRestartApp = useMemo(() => state.activities.length > 0, [state.activities]);
+
   return (
     <>
       <header className="bg-lime-600 py-3">
         <div className="px-5 max-w-4xl mx-auto flex justify-between">
           <h1 className="text-center text-lg font-bold text-white uppercase">Contador de Calorías</h1>
+          <button className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white hover:cursor-pointer rounded-lg text-sm disabled:hover:cursor-auto disabled:opacity-10" disabled={!canRestartApp} onClick={() => dispatch({ type: 'restart-app' })}>Reiniciar App</button>
         </div>
       </header>
 
--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -3,7 +3,8 @@ import type { Activity } from "../types"
 export type ActivityActions = 
   { type: 'save-activity', payload: { newActivity: Activity } } |
   { type: 'set-activity', payload: { id: Activity['id'] } } |
-  { type: 'delete-activity', payload: { id: Activity['id'] } };
+  { type: 'delete-activity', payload: { id: Activity['id'] } } |
+  { type: 'restart-app' };
 
 export interface ActivityState {
   activities: Activity[];
@@ -51,6 +52,13 @@ export const activityReducer = (
     return {
       ...state,
       activities: state.activities.filter(activity => activity.id !== action.payload.id)
+    }
+  }
+
+  if(action.type === 'restart-app') {
+    return {
+      activities: [],
+      activeId: ''
     }
   }
 
